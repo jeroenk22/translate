b470d2eef774acaf60c5bd52f28e7a7eed1f39f5
Jeroen <jeroenkrajenbrink@gmail.com>
2019-05-06 16:56:09 +0200

    Added ColorContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -23,9 +24,13 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange('dutch')}
           />
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="primary">
+          {' '}
+          {/* The order of surrounding providers doesn't matter! */}
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
