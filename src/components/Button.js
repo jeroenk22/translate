e765be9e6e9cd9df55a35bfee2f34cb20e37b9a1
Jeroen <jeroenkrajenbrink@gmail.com>
2019-05-06 16:43:08 +0200

    Refactored Button.js. Using Consumer instead of this.context

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,18 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Verzenden';
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Verzenden';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
