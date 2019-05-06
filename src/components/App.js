c9451c2d2fca82b277392c7fea156d70b1171daf
Jeroen <jeroenkrajenbrink@gmail.com>
2019-05-06 15:24:47 +0200

    Added Semantic-UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
-        return <div>App</div>;
-    }
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
 }
 
 export default App;
